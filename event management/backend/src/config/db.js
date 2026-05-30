9fdd938c0e915c114ddffb88d32d61a19914b95c
Ritam Saha <ritamsaha52003@gmail.com>
2026-05-30 15:10:52 +0530

    Add SSL for Railway MySQL

--- event management/backend/src/config/db.js
+++ event management/backend/src/config/db.js
@@ -5,10 +5,15 @@ dotenv.config();
 
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // IMPORTANT
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   connectTimeout: 60000,
