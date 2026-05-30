4755f0b841a2bb5af83dd69fb2a5a0e03445f3b4
Ritam Saha <ritamsaha52003@gmail.com>
2026-05-30 15:06:08 +0530

    debug railway

--- event management/backend/src/config/db.js
+++ event management/backend/src/config/db.js
@@ -24,5 +24,9 @@ async function testConnection() {
     process.exit(1);
   }
 }
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 
 testConnection();
